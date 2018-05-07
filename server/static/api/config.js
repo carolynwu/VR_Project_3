35b1c98a82b3e5e3d0ec5b1c0343de73e3b1d860
swoldemi <b30bd351371c686298d32281b337e8@gmail.com>
2018-05-06 20:57:14 -0500

    Note: Committed invalid API keys. Do not use

--- server/static/api/config.js
+++ server/static/api/config.js
@@ -1,4 +1,5 @@
 function FrameConfig (type) {
 	this.type = type;
-	this.key = "AIzaSyDds3KouOQ0eh4T-Wux6Uav2m5UFid2v4s"
+	this.youtube_key = "AIzaSyDds3KouOQ0eh4T-Wux6Uav2m5UFid2v4s";
+	this.flickr_key = "17229c54c0b1b78178f69632b3648b4e";
 }
